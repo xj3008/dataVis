cc0a6695944275ff931add94ac11c5611ad67025
Evelyn <xiongjing3001@gmail.com>
2020-04-25 23:56:05 -0400

    adjust style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Header.css";
 export default function Header() {
-  return <div className="header">COVID-19 Cases in U.S</div>;
+  return (
+    <div className="header">
+      <p>COVID-19 Cases in U.S</p>
+    </div>
+  );
 }
